2dcdca74bf01260f1cea608a3b8a4823a0830d0f
ulasberk <85496179+berkulasch@users.noreply.github.com>
2024-10-23 22:13:29 +0200

    Initial Commit

---
+++ TippMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TippMe
+//
+//  Created by Berk Ulas on 23.10.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TippMe/TippMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TippMeApp.swift
+//  TippMe
+//
+//  Created by Berk Ulas on 23.10.24.
+//
+
+import SwiftUI
+
+@main
+struct TippMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
